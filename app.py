12b31489eb27f8153b94617284661ea7ce98d6f4
kass173 <kass17_3@hotmail.com>
2022-12-18 14:40:04 +0000

    Update

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 #################################################
 # Database Setup
@@ -23,8 +24,9 @@ engine = create_engine("sqlite:///Resources/cost_of_living.sqlite")
 @app.route("/")
 def index():
     """Return the homepage."""
-    return render_template("index.html")
-@app.route("/Cost of Living")
+    return render_template("index2.html")
+
+@app.route("/cost-of-living")
 def Country():
     """Return a list of country names."""
  # Create our session (link) from Python to the DB
@@ -35,7 +37,10 @@ def Country():
         # Query all passengers
     # cost_of_living2022 = session.query(Cost_of_living.Country, Cost_of_living.Cost_of_Living_Index, Cost_of_living.Year).all()
     data = pd.read_sql("SELECT * FROM cost_of_living", engine)
+    data = data.to_json(orient="records")
     # Return a list of the column names (sample names)
-    return jsonify(list(cost_of_living2022.columns)[2:])
+    return render_template("index2.html", data=data)
+    
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,43 @@
+//var myMap = heatmap("cost_of_living", {
+//    center: [37.7749, -122.4194],
+//    zoom: 13
+//});
+var myMap = JSON.parse(data);
+
+    // loop through the data and push the lat and long to the heatArray
+for (var i = 0; i < data.length; i++) {
+    var location = data[i].location;
+    if (location) {
+            heatArray.push([location.coordinates[1], location.coordinates[0]]); // lat and long are reversed in the data
+        }
+    }
+
+
+
+    
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000"
+
+d3.json(url).then(function (data) {
+    console.log(data);
+
+    var heatArray = [];
+
+    // loop through the data and push the lat and long to the heatArray
+    for (var i = 0; i < data.length; i++) {
+        var location = data[i].location;
+        if (location) {
+            heatArray.push([location.coordinates[1], location.coordinates[0]]); // lat and long are reversed in the data
+        }
+    }
+    
+     // create the heatmap layer
+     var heat = L.heatLayer(heatArray, {
+        radius: 20,
+        blur: 35
+    }).addTo(myMap);
+});
